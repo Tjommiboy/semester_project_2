820e685b8240ce0743e293445ae0793d0255a60a
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2024-05-12 23:45:23 +0700

    adds apikey

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,5 +1,26 @@
+export const API_KEY = "1d63e6d7-3501-4b3a-8a83-d8834faba43a";
+
 export const API_BASE = "https://v2.api.noroff.dev";
 export const API_AUTH = "/auth";
 export const API_REGISTER = "/register";
 export const API_LOGIN = "/login";
-export const apiKey = "/create-api-key";
+
+// export const apiKey = "/create-api-key";
+
+// export async function getAPIkey() {
+//   const response = await fetch(API_BASE + API_AUTH + apiKey, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${load("token")}`,
+//     },
+//     body: JSON.stringify({
+//       name: "test key",
+//     }),
+//   });
+//   if (response.ok) {
+//     return await response.json();
+//   }
+//   console.error(await response.json());
+//   throw new Error("Failed to get API key");
+// }
